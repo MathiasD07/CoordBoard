7f9a24b447dfdfc575da907ae48a0ba782e1be05
BuildTools <unconfigured@null.spigotmc.org>
2022-12-07 15:48:48 +0100

    feat: create broadcast command

--- src/main/java/fr/forky/coordboard/Main.java
+++ src/main/java/fr/forky/coordboard/Main.java
@@ -1,5 +1,8 @@
 package fr.forky.coordboard;
 
+import fr.forky.coordboard.commands.CommandBroadcast;
+import fr.forky.coordboard.utils.commands.SimpleCommand;
+import org.bukkit.craftbukkit.v1_19_R1.CraftServer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -7,5 +10,17 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         System.out.println("ça marche !");
+
+        createCommand(new SimpleCommand(
+                "broadcast",
+                "Envoyer un message à tout le serveur",
+                new CommandBroadcast(),
+                "bc"
+        ));
+    }
+
+    private void createCommand(SimpleCommand command) {
+        CraftServer server = (CraftServer) getServer();
+        server.getCommandMap().register(getName(), command);
     }
 }
